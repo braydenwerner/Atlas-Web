7138effeb2fdc4cd850870be3b9290f59cf0c821
braydenwerner <braydenwerner0@gmail.com>
2021-09-10 17:05:21 -0700

    removed unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Nav } from './components'
 import { Home, PrivacyPolicy } from './pages'
